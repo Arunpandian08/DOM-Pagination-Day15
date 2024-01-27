c316707b7549c89be3df9857286c2cd85d840ff3
Arunpandian08 <arunpandianda123@example.com>
2024-01-27 17:34:05 +0530

    task completed

---
+++ js/script.js
@@ -0,0 +1,705 @@
+var UserData = [
+  {
+    Id: "1",
+    Name: "Lura Senger",
+    Email: "Xander_Collier@yahoo.com",
+  },
+  {
+    Id: "2",
+    Name: "Wilburn Weber",
+    Email: "Bennett_Kreiger11@yahoo.com",
+  },
+  {
+    Id: "3",
+    Name: "Tyrique Hahn",
+    Email: "Juston.Altenwerth@yahoo.com",
+  },
+  {
+    Id: "4",
+    Name: "Lenny Bailey",
+    Email: "Guiseppe_Hegmann@yahoo.com",
+  },
+  {
+    Id: "5",
+    Name: "Vladimir Keeling",
+    Email: "Louisa_Walsh18@hotmail.com",
+  },
+  {
+    Id: "6",
+    Name: "Kellie Crona",
+    Email: "Chandler_Abernathy@yahoo.com",
+  },
+  {
+    Id: "7",
+    Name: "Carolina White",
+    Email: "Royal50@hotmail.com",
+  },
+  {
+    Id: "8",
+    Name: "Alfredo Conn",
+    Email: "Clarabelle34@hotmail.com",
+  },
+  {
+    Id: "9",
+    Name: "Stan Shanahan",
+    Email: "Lamar.McClure@hotmail.com",
+  },
+  {
+    Id: "10",
+    Name: "Marvin Fay",
+    Email: "Osbaldo58@hotmail.com",
+  },
+  {
+    Id: "11",
+    Name: "Torrance Rau",
+    Email: "Orin45@gmail.com",
+  },
+  {
+    Id: "12",
+    Name: "Harold Gutmann MD",
+    Email: "Alyce.Stracke37@yahoo.com",
+  },
+  {
+    Id: "13",
+    Name: "Taryn Torphy",
+    Email: "Dean_Breitenberg71@hotmail.com",
+  },
+  {
+    Id: "14",
+    Name: "Bryana Lang",
+    Email: "Tatum.Ullrich@yahoo.com",
+  },
+  {
+    Id: "15",
+    Name: "Nyasia Green DDS",
+    Email: "Dino83@gmail.com",
+  },
+  {
+    Id: "16",
+    Name: "Nasir Gerhold",
+    Email: "Lilian_Bashirian8@hotmail.com",
+  },
+  {
+    Id: "17",
+    Name: "Raymundo Ritchie PhD",
+    Email: "Antwan.Schoen15@yahoo.com",
+  },
+  {
+    Id: "18",
+    Name: "Delmer Marvin",
+    Email: "Kiera62@yahoo.com",
+  },
+  {
+    Id: "19",
+    Name: "Rachel Wilkinson",
+    Email: "Foster_Conroy@gmail.com",
+  },
+  {
+    Id: "20",
+    Name: "Gladys Howell",
+    Email: "Constance.Labadie10@yahoo.com",
+  },
+  {
+    Id: "21",
+    Name: "Ciara Klocko",
+    Email: "Harvey76@yahoo.com",
+  },
+  {
+    Id: "22",
+    Name: "Quentin O'Kon",
+    Email: "Amely_Cummings2@yahoo.com",
+  },
+  {
+    Id: "23",
+    Name: "Ms. Gabriella Kunde",
+    Email: "Lorenza_Cummerata@hotmail.com",
+  },
+  {
+    Id: "24",
+    Name: "Gerald Reilly",
+    Email: "Lia_Konopelski@gmail.com",
+  },
+  {
+    Id: "25",
+    Name: "Brody Carter I",
+    Email: "Colten.Wilderman90@hotmail.com",
+  },
+  {
+    Id: "26",
+    Name: "Alanis Klocko",
+    Email: "Johathan.Champlin69@gmail.com",
+  },
+  {
+    Id: "27",
+    Name: "Caroline Miller",
+    Email: "Delaney.Kozey74@gmail.com",
+  },
+  {
+    Id: "28",
+    Name: "Ms. Merritt McClure",
+    Email: "Wendy.Zieme@gmail.com",
+  },
+  {
+    Id: "29",
+    Name: "Jovani Schoen",
+    Email: "Norval_Zieme@hotmail.com",
+  },
+  {
+    Id: "30",
+    Name: "Berniece Bradtke",
+    Email: "Okey.Fisher39@hotmail.com",
+  },
+  {
+    Id: "31",
+    Name: "Hanna Gottlieb",
+    Email: "Edna62@hotmail.com",
+  },
+  {
+    Id: "32",
+    Name: "Christiana Bergnaum",
+    Email: "Donnie.Paucek22@gmail.com",
+  },
+  {
+    Id: "33",
+    Name: "ReId Stehr",
+    Email: "Hiram69@gmail.com",
+  },
+  {
+    Id: "34",
+    Name: "Evans Ward",
+    Email: "Liliane72@hotmail.com",
+  },
+  {
+    Id: "35",
+    Name: "Miss Enos Davis",
+    Email: "Kolby.Grady@hotmail.com",
+  },
+  {
+    Id: "36",
+    Name: "Mathew Lueilwitz",
+    Email: "Mariah.Hagenes@yahoo.com",
+  },
+  {
+    Id: "37",
+    Name: "Romaine Robel",
+    Email: "Sheila19@yahoo.com",
+  },
+  {
+    Id: "38",
+    Name: "Immanuel Aufderhar",
+    Email: "Aileen32@hotmail.com",
+  },
+  {
+    Id: "39",
+    Name: "Julius Reilly Jr.",
+    Email: "Santa89@yahoo.com",
+  },
+  {
+    Id: "40",
+    Name: "Linnea Borer",
+    Email: "Jarrett_Hegmann2@hotmail.com",
+  },
+  {
+    Id: "41",
+    Name: "Misael Abernathy IV",
+    Email: "Robbie42@yahoo.com",
+  },
+  {
+    Id: "42",
+    Name: "Mr. Gerda Jerde",
+    Email: "Gustave36@hotmail.com",
+  },
+  {
+    Id: "43",
+    Name: "Easton Daugherty",
+    Email: "Malcolm.Schuster11@hotmail.com",
+  },
+  {
+    Id: "44",
+    Name: "Leanna Gutkowski",
+    Email: "Chelsea9@gmail.com",
+  },
+  {
+    Id: "45",
+    Name: "Rollin Trantow",
+    Email: "Marques_Hickle26@gmail.com",
+  },
+  {
+    Id: "46",
+    Name: "Heaven Luettgen",
+    Email: "Lonnie.Greenfelder95@hotmail.com",
+  },
+  {
+    Id: "47",
+    Name: "Gilda Huel",
+    Email: "Stephania.Hintz@gmail.com",
+  },
+  {
+    Id: "48",
+    Name: "Javon Walker",
+    Email: "Lamar_Bailey39@yahoo.com",
+  },
+  {
+    Id: "49",
+    Name: "Stewart King",
+    Email: "Chasity_Jenkins82@gmail.com",
+  },
+  {
+    Id: "50",
+    Name: "Cortez Lueilwitz",
+    Email: "Lelia.Mitchell43@gmail.com",
+  },
+  {
+    Id: "51",
+    Name: "Ora Kilback",
+    Email: "Matt25@gmail.com",
+  },
+  {
+    Id: "52",
+    Name: "Lera Macejkovic",
+    Email: "Jamal.Donnelly45@hotmail.com",
+  },
+  {
+    Id: "53",
+    Name: "Asa Gottlieb",
+    Email: "Carolyne.Langosh@yahoo.com",
+  },
+  {
+    Id: "54",
+    Name: "Avery Nienow",
+    Email: "Hudson_Bernhard47@yahoo.com",
+  },
+  {
+    Id: "55",
+    Name: "Dr. Katrine Ryan",
+    Email: "Colten40@gmail.com",
+  },
+  {
+    Id: "56",
+    Name: "FlorIda Kulas",
+    Email: "Alejandra_Bernhard@yahoo.com",
+  },
+  {
+    Id: "57",
+    Name: "Mauricio Gerlach",
+    Email: "Vincenza_Gusikowski84@hotmail.com",
+  },
+  {
+    Id: "58",
+    Name: "Chadrick Simonis",
+    Email: "Reymundo.Doyle@gmail.com",
+  },
+  {
+    Id: "59",
+    Name: "Beau Jenkins",
+    Email: "Cletus65@hotmail.com",
+  },
+  {
+    Id: "60",
+    Name: "Camilla Cole",
+    Email: "Eda48@yahoo.com",
+  },
+  {
+    Id: "61",
+    Name: "Abagail MacGyver",
+    Email: "Jevon.Koch@hotmail.com",
+  },
+  {
+    Id: "62",
+    Name: "Ludwig Prohaska",
+    Email: "Eldon_DuBuque37@yahoo.com",
+  },
+  {
+    Id: "63",
+    Name: "Berenice Hane",
+    Email: "Lonnie3@gmail.com",
+  },
+  {
+    Id: "64",
+    Name: "Abel Durgan",
+    Email: "Marlee.Weimann42@gmail.com",
+  },
+  {
+    Id: "65",
+    Name: "Tate Kuhlman",
+    Email: "Hope31@yahoo.com",
+  },
+  {
+    Id: "66",
+    Name: "Mrs. Hermina Daniel",
+    Email: "Murphy59@yahoo.com",
+  },
+  {
+    Id: "67",
+    Name: "Marcelino Nienow",
+    Email: "Estefania_Dibbert97@hotmail.com",
+  },
+  {
+    Id: "68",
+    Name: "Raphaelle Connelly",
+    Email: "Josie.Legros70@yahoo.com",
+  },
+  {
+    Id: "69",
+    Name: "Mina Heaney",
+    Email: "Pete.Hermann79@hotmail.com",
+  },
+  {
+    Id: "70",
+    Name: "JaIda Torp",
+    Email: "Jonatan82@yahoo.com",
+  },
+  {
+    Id: "71",
+    Name: "Megane Fadel",
+    Email: "Dandre82@yahoo.com",
+  },
+  {
+    Id: "72",
+    Name: "Angelo Schimmel V",
+    Email: "Frederic54@hotmail.com",
+  },
+  {
+    Id: "73",
+    Name: "Jaren Ferry DDS",
+    Email: "Kristian.Kilback@yahoo.com",
+  },
+  {
+    Id: "74",
+    Name: "Annette O'Connell",
+    Email: "Domenica.Christiansen@gmail.com",
+  },
+  {
+    Id: "75",
+    Name: "Elenor Rohan PhD",
+    Email: "Alvera.Huel48@yahoo.com",
+  },
+  {
+    Id: "76",
+    Name: "Linnie Beier",
+    Email: "Reece_Berge@gmail.com",
+  },
+  {
+    Id: "77",
+    Name: "Maybelle Lehner",
+    Email: "Arlo.Hagenes@gmail.com",
+  },
+  {
+    Id: "78",
+    Name: "Hailie Haag",
+    Email: "Edythe.Lind59@hotmail.com",
+  },
+  {
+    Id: "79",
+    Name: "Marcelina Schuppe",
+    Email: "Kip.Cummerata77@gmail.com",
+  },
+  {
+    Id: "80",
+    Name: "Ezra Lang",
+    Email: "Garret.Swaniawski@hotmail.com",
+  },
+  {
+    Id: "81",
+    Name: "Carey Kiehn",
+    Email: "Sabrina70@gmail.com",
+  },
+  {
+    Id: "82",
+    Name: "Carole Aufderhar",
+    Email: "Efrain.Schamberger77@hotmail.com",
+  },
+  {
+    Id: "83",
+    Name: "Fanny Beatty",
+    Email: "Ewald28@yahoo.com",
+  },
+  {
+    Id: "84",
+    Name: "Mina Lubowitz",
+    Email: "Vincenzo.Herman@gmail.com",
+  },
+  {
+    Id: "85",
+    Name: "Anabelle Thiel",
+    Email: "Amely.Renner@hotmail.com",
+  },
+  {
+    Id: "86",
+    Name: "Gerry Homenick",
+    Email: "Ada_Von54@hotmail.com",
+  },
+  {
+    Id: "87",
+    Name: "Domenico Maggio",
+    Email: "Freeman.Stamm@yahoo.com",
+  },
+  {
+    Id: "88",
+    Name: "Mauricio Bradtke III",
+    Email: "Alford83@yahoo.com",
+  },
+  {
+    Id: "89",
+    Name: "Bulah Yost III",
+    Email: "Nicolas_Schimmel6@yahoo.com",
+  },
+  {
+    Id: "90",
+    Name: "Orville Dibbert DDS",
+    Email: "Bryana_Herzog@yahoo.com",
+  },
+  {
+    Id: "91",
+    Name: "Autumn Osinski",
+    Email: "Davin8@hotmail.com",
+  },
+  {
+    Id: "92",
+    Name: "Levi Gibson",
+    Email: "Deangelo69@gmail.com",
+  },
+  {
+    Id: "93",
+    Name: "Reva McKenzie",
+    Email: "Bret40@hotmail.com",
+  },
+  {
+    Id: "94",
+    Name: "Jensen Pfannerstill",
+    Email: "Dax_Marquardt@hotmail.com",
+  },
+  {
+    Id: "95",
+    Name: "Tyrese Larson MD",
+    Email: "Cecelia85@yahoo.com",
+  },
+  {
+    Id: "96",
+    Name: "Dayne Sanford",
+    Email: "Tyrique_Waters16@hotmail.com",
+  },
+  {
+    Id: "97",
+    Name: "Jeramy Steuber",
+    Email: "Catalina.Stehr24@yahoo.com",
+  },
+  {
+    Id: "98",
+    Name: "Madge Bogisich",
+    Email: "Joey.Marquardt50@gmail.com",
+  },
+  {
+    Id: "99",
+    Name: "Casimer MacGyver",
+    Email: "Julius.Bednar96@yahoo.com",
+  },
+  {
+    Id: "100",
+    Name: "Arlene Kassulke",
+    Email: "Josiane_Smith@yahoo.com",
+  },
+];
+
+let itemsPerPage = 10;
+let totalPages = UserData.length;
+
+const firstBtn = document.querySelector(".first");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const lastBtn = document.querySelector(".last");
+
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const btn4 = document.querySelector(".btn-4");
+const btn5 = document.querySelector(".btn-5");
+const btn6 = document.querySelector(".btn-6");
+const btn7 = document.querySelector(".btn-7");
+const btn8 = document.querySelector(".btn-8");
+const btn9 = document.querySelector(".btn-9");
+const btn10 = document.querySelector(".btn-10");
+
+// To set a value for current page
+
+
+let currentItem = 1;
+
+// Try to load the data for the first page when the page loads
+window.addEventListener("DOMContentLoaded", () => {
+  showPage(currentItem, UserData);
+  updatePagination(totalPages, currentItem);
+});
+
+// Logic for previous user when the user clicks on the button
+prevBtn.addEventListener("click", () => {
+  // Decrement current position by 10
+  if (0 < currentItem) {
+    currentItem -= itemsPerPage;
+    showPage(currentItem / itemsPerPage + 1, UserData);
+    updatePagination(totalPages, currentItem / itemsPerPage + 1);
+  } else {
+    alert("Hey Budyy! You are already on the first page.");
+  }
+});
+
+// Logic for next page when the user clicks on the next button
+nextBtn.addEventListener("click", () => {
+  // Increment current position by 10
+  if (totalPages - itemsPerPage > currentItem) {
+    currentItem += itemsPerPage;
+    showPage(currentItem / itemsPerPage + 1, UserData);
+    updatePagination(totalPages, currentItem / itemsPerPage + 1);
+  } else {
+    alert("hey Buddy! You are in the last page.");
+  }
+});
+
+// Logic for first button
+firstBtn.addEventListener("click", () => {
+  if (currentItem > 0) {
+    currentItem = 0;
+    showPage(1, UserData);
+    updatePagination(totalPages, 1);
+  } else {
+    alert("Hey Budyy! You are already on the first page.");
+  }
+});
+
+// Logic for last button
+lastBtn.addEventListener("click", () => {
+  const lastPage = Math.ceil(totalPages / itemsPerPage);
+  const lastItemIndex = totalPages - (totalPages % itemsPerPage || itemsPerPage);
+
+  if (currentItem < lastItemIndex) {
+    currentItem = lastItemIndex;
+    showPage(lastPage, UserData);
+    updatePagination(lastPage, lastPage);
+  } else {
+    alert("Hey Budyy! You are already on the last page.");
+  }
+});
+
+// const btn1 = document.querySelector("btn-1");
+btn1.addEventListener("click", () => {
+  currentItem = 0;
+  showPage(1, UserData);
+  updatePagination(totalPages, 1);
+});
+// const btn2 = document.querySelector("btn-2");
+btn2.addEventListener("click", () => {
+  currentItem = itemsPerPage;
+  showPage(2, UserData);
+  updatePagination(totalPages, 2);
+});
+// const btn3 = document.querySelector("btn-3");
+btn3.addEventListener("click",()=> {
+    currentItem = itemsPerPage * 2;
+    showPage(3, UserData);
+    updatePagination(totalPages, 3);
+  });
+// const btn4 = document.querySelector("btn-4");
+btn4.addEventListener("click", () => {
+  currentItem = itemsPerPage * 3;
+  showPage(4, UserData);
+  updatePagination(totalPages, 4);
+});
+// const btn5 = document.querySelector("btn-5");
+btn5.addEventListener("click", () => {
+  currentItem = itemsPerPage * 4;
+  showPage(5, UserData);
+  updatePagination(totalPages, 5);
+});
+// const btn6 = document.querySelector("btn-6");
+btn6.addEventListener("click", () => {
+ currentItem = itemsPerPage * 5;
+  showPage(6, UserData);
+  updatePagination(totalPages, 6);
+});
+// const btn7 = document.querySelector("btn-7");
+btn7.addEventListener("click", () => {
+  currentItem = itemsPerPage * 6;
+  showPage(7, UserData);
+  updatePagination(totalPages, 7);
+});
+// const btn8 = document.querySelector("btn-8");
+btn8.addEventListener("click", () => {
+  currentItem = itemsPerPage * 7;
+  showPage(8, UserData);
+  updatePagination(totalPages, 8);
+});
+// const btn9 = document.querySelector("btn-9");
+btn9.addEventListener("click", () => {
+  currentItem = itemsPerPage * 8;
+  showPage(9, UserData);
+  updatePagination(totalPages, 9);
+});
+
+
+// const btn10 = document.querySelector("btn-10");
+btn10.addEventListener("click", () => {
+  currentItem = itemsPerPage * 9;
+  showPage(10, UserData);
+  updatePagination(totalPages, 10);
+});
+
+const page = document.querySelector(".pageChanges");
+
+function showPage(pageNumber, Data) {
+  const startIndex = (pageNumber - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = Data.slice(startIndex, endIndex);
+  createTable(itemsToDisplay);
+  page.innerHTML = "Current Page: " + pageNumber;
+
+}
+ 
+function createTable(data) {
+  const tableContainer = document.querySelector(".table-responsive");
+  const table = document.createElement("table");
+  table.className="table table-bordered";
+  table.id="table";
+
+  const head = document.createElement("thead")
+  const headRow = document.createElement("tr");
+
+
+  // Create table header
+  Object.keys(data[0]).forEach((key) => {
+    const th = document.createElement("th");
+    th.textContent = key;
+    headRow.appendChild(th);
+  });
+
+  head.appendChild(headRow)
+  table.appendChild(head);
+
+  const body = document.createElement("tbody")
+  // Create table rows
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+    Object.values(item).forEach((value) => {
+      const td = document.createElement("td");
+      td.textContent = value;
+      row.appendChild(td);
+    });
+    body.appendChild(row);
+  });
+table.appendChild(body);
+
+  // Clear previous content and append the new table
+  tableContainer.innerHTML = "";
+  tableContainer.appendChild(table);
+}
+
+function updatePagination(totalPages, currentPage) {
+  // Update the active class for pagination buttons
+  for (let i = 1; i <= 10; i++) {
+    const btn = document.querySelector(`#btn${i}`);
+    if (i === currentPage) {
+      btn.classList.add("active");
+    } else {
+      btn.classList.remove("active");
+    }
+  }
+}
